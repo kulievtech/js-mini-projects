966bbb475bb207d07ca253fb36498756c70c0553
Tohir Kuliev <kuliev.tech@gmail.com>
2023-10-22 10:14:23 -0400

    add sort feature to a bankist app

--- arrays-bankist/script.js
+++ arrays-bankist/script.js
@@ -59,10 +59,14 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = "";
 
-    movements.forEach(function (mov, i) {
+    // using slice for optimal shallow copy and to keep continue chaining
+
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function (mov, i) {
         const type = mov > 0 ? "deposit" : "withdrawal";
 
         const html = `
@@ -186,6 +190,25 @@ btnTransfer.addEventListener("click", function (event) {
     }
 });
 
+btnLoan.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const amount = Number(inputLoanAmount.value);
+
+    if (
+        amount > 0 &&
+        currentAccount.movements.some((mov) => mov >= amount * 0.1)
+    ) {
+        // Add movement
+        currentAccount.movements.push(amount);
+
+        // Update UI
+        updateUI(currentAccount);
+    }
+
+    inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (event) {
     event.preventDefault();
 
@@ -207,14 +230,13 @@ btnClose.addEventListener("click", function (event) {
     inputCloseUsername.value = inputClosePin.value = "";
 });
 
+let sorted = false;
+
+btnSort.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
+
 // LECTURES
-
-const currencies = new Map([
-    ["USD", "United States dollar"],
-    ["EUR", "Euro"],
-    ["GBP", "Pound sterling"]
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
