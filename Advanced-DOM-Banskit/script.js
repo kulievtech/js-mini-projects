e2592d37a3e6d07d7d9d5deb61144a1fa6f5fbfe
Tohir Kuliev <kuliev.tech@gmail.com>
2023-10-25 12:39:43 -0400

    Add Menu Fade Navigation

--- Advanced-DOM-Banskit/script.js
+++ Advanced-DOM-Banskit/script.js
@@ -8,6 +8,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 const openModal = function (event) {
     event.preventDefault();
@@ -81,10 +85,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 
 // Tabbed Component
 
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", function (e) {
     const clicked = e.target.closest(".operations__tab");
 
@@ -108,6 +108,28 @@ tabsContainer.addEventListener("click", function (e) {
         .querySelector(`.operations__content--${clicked.dataset.tab}`)
         .classList.add("operations__content--active");
 });
+
+// Menu Fade Navigation - passing arguments to Event Handler
+
+const handleHover = function (e) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target;
+
+        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+
+        const logo = link.closest(".nav").querySelector("img");
+
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 // Lectures
 
